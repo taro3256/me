e6035bbb2d889d3a2ae805a7d24b92fcc6b39401
taro <taro@taros-MacBook-Pro.local>
2022-04-12 21:46:19 +0900

    basePathを修正

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.GITHUB_ACTIONS ? "/me" : "",
+  basePath: process.env.GITHUB_ACTIONS ? "/" : "",
   trailingSlash: true,
 }
 
